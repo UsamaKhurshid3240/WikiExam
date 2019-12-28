16125f72597c493142e0f745fc5971e7efcf2df5
UsamaKhurshid3240 <55357938+UsamaKhurshid3240@users.noreply.github.com>
2019-12-28 08:17:31 -0800

    Update protractor.conf.js

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,5 +1,5 @@
 // Protractor configuration file, see link for more information
-// https://github.com/angular/protractor/blob/master/lib/config
+// https://github.com/angular/protractor/blob/master/lib/config.ts
 
 const { SpecReporter } = require('jasmine-spec-reporter');
 
